394f382c10a733039cde44756fa718adcedbb4a1
yagompassos <yagompassos@gmail.com>
2023-10-30 21:52:44 -0300

    adicionando print do 1222

---
+++ Short_Story_Competition_1222/1222.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXSIZE 70
+
+void main () {
+
+	int i, l, c, n, x;
+	int linhas, paginas, letras;
+	char palavra[MAXSIZE + 1];
+
+	while (scanf("%d %d %d", &n, &l, &c) != EOF) {
+    	paginas = linhas = 1;
+    	scanf("%s", palavra);
+    	letras = strlen(palavra);
+
+		n--;
+		for (i = 0; i < n; ++i)	{
+
+			scanf("%s", palavra);
+
+			x = strlen(palavra);
+
+			if ((letras + x + 1) <= c)
+				letras += x + 1;
+			else {
+				++linhas;
+				if (linhas > l)
+					++paginas, linhas = 1;
+
+				letras = x;
+
+			}
+
+		}
+
+		printf("%d\n", paginas);
+
+	}
+
+}
